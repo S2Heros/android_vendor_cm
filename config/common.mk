586b59bd6b458d20d4051b1875861a7f4402ad3f
MNT <m.zilius@gmail.com>
2013-08-16 13:55:52 +0300

    Derp

--- config/common.mk
+++ config/common.mk
@@ -517,18 +517,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 
 -include $(WORKSPACE)/hudson/image-auto-bits.mk
-
-else
-    ifeq ($(PRODUCT_VERSION_MINOR),0)
-        CM_VERSION := $(PRODUCT_VERSION_MAJOR)-$(shell date -u +%Y%m%d)-$(CM_BUILDTYPE)-$(CM_BUILD)$(CM_EXTRAVERSION)
-    else
-        CM_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(CM_BUILDTYPE)-$(CM_BUILD)$(CM_EXTRAVERSION)
-    endif
-endif
-
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.cm.version=$(CM_VERSION) \
-  ro.modversion=$(CM_VERSION)
-
-
--include $(WORKSPACE)/hudson/image-auto-bits.mk
