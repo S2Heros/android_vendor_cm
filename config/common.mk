b31fe70c93f507d839e882f12cdd971ce4892ef3
MNT <mnt@rootbox.ca>
2013-08-24 15:53:05 +0300

    Hybrid preparation (1/x)

--- config/common.mk
+++ config/common.mk
@@ -138,6 +138,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/mkshrc:system/etc/mkshrc
 
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/$(TARGET_PRODUCT).conf:system/etc/cyanfox/properties.conf
+
 # T-Mobile theme engine
 include vendor/cm/config/themes_common.mk
 
